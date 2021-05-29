e88acfb256837062c0cf7a1a5825a2ddea1ee44e
Chulhwa Han <cjfghk5697@gmail.com>
2021-05-29 11:50:00 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
-function inputlogin(){
-    const id
+function printlogin(){
+    const ID=getElementById('ID').value;
+    document.getElementById('input_ID').innerText=ID;
+    const Ps=document.getElementById('Ps').value
+    document.getElementById('input_ID').innerText=ID;
 }
+
+function login(){
+    const ID=document.getElementById('ID').value;
+    const Ps=document.getElementById('Ps').value;
+    return ID + Ps;
+}
+
+var alterName=function(){
+    var realIdPs=login();
+    alert(realIdPs);
+}
+
