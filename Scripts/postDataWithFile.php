4c390f48cdcc1f542f38e66f70311fdbdc203b52
Chulhwa Han <cjfghk5697@gmail.com>
2021-06-23 00:20:41 +0900

    db연결
    db연결과 뒤로가기 등 다양한 기능 다 구현 완료

--- Scripts/postDataWithFile.php
+++ Scripts/postDataWithFile.php
@@ -1,8 +1,7 @@
 <?php
  
     header("Content-Type: text/html; charset=UTF-8");
-    ini_set( "display_errors", 1 );
-
+ 
     $name= $_POST['name'];
     $message= $_POST['msg'];
  
@@ -27,11 +26,9 @@
     $fileName= date('Ymdhis').$srcName;
  
     //최종 이지미 파일의 경로
-    $dstName= "$path . $fileName";
+    $dstName= $path . $fileName;
     $result=move_uploaded_file($tmpName, $dstName);
-    if($result) echo "success upload file.";
-    else echo "fail upload file";
- 
+
     //Data 저장하는 날짜와 시간
     $now= date('Y-m-d h:m:s');
  
@@ -41,30 +38,19 @@
     //php에서 Database서버와 연동하기
     //MySQL DB에 접속하기!!
     $conn=mysqli_connect("localhost","cjfghk5697","han020615!","cjfghk5697"); //DB 서버 주소, DB 접속 아이디, DB접속 비번, DB명 (파일명) 
+ 
     //한글 깨짐 방지
     mysqli_query($conn,"set names utf8");
-
+ 
     //SQL 쿼리문 작성
-    $sql="INSERT INTO board(name,message, file, date) VALUES ('$name','$message','$dstName','$now')";
-
-    #query = "INSERT INTO tblGames (name, description, image) VALUES ('$name', '$description', '". $target_path ."')";
-    
+    $sql="insert into board(name,message, file, date) values('$name','$message','$dstName','$now')";
     $result=mysqli_query($conn,$sql);
-    if ($result === false) { // false가 나왔다면 무슨 에러인지 출력한다(29번 줄의  태그를 주석 쳐야 제대로 볼 수 있다)
-        echo mysqli_error($conn);
-      }
-    if($result){
-        echo "insert success"; 
-        header("Location: http://cjfghk5697.dothome.co.kr/"); 
-   
-    } else{
-        echo "insert fail";
-    }
+ 
+    echo "<script>location.href='/index.html'</script>";
  
     mysqli_close($conn);
  
 ?>
  
  
-
 
